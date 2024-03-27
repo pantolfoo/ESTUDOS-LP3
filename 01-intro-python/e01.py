4741a611be4fb90ee9b574dc20228b67fb42daad
pantolfoo <isabella.pantolfo@aluno.ifsp.edu.br>
2024-03-26 23:34:11 -0300

    Create e01.py

---
+++ 01-intro-python/e01.py
@@ -0,0 +1,5 @@
+#escreva um programa que solicita ao usuario um numero inteiro e apresenta seu antecessor e seu sucessor
+
+numero = int(input ('digite um número\n'))
+print('antecessor do número digitado: ', numero-1)
+print('sucessor do número digitado: ', numero+1)
